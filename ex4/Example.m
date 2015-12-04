08274383663f4e76d012aa74f76a5f7c42bf0d6a
Hill Lo <hilllocanton@gmail.com>
2015-12-03 19:43:24 -0500

    upload sample code

---
+++ ex4/Example.m
@@ -0,0 +1,10 @@
+Theta1 = ones(10,11)
+Theta2 = 2*ones(10,11)
+Theta3 = 3*ones(1,11)
+
+thetaVec = [Theta1(:);Theta2(:);Theta3(:)];
+size (thetaVec) % test
+
+reshape(thetaVec(1:110),10,11)
+reshape(thetaVec(111:220),10,11)
+reshape(thetaVec(221:231),1,11)
